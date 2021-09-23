e9feae342aa9b7f3ae74382abb35e9b317bb00e1
Jason <kangja1996@gmail.com>
2021-09-22 23:33:19 -0400

    finished project

--- src/Coin.js
+++ src/Coin.js
@@ -20,16 +20,17 @@ const Coin = ({
         </div>
         <div className="coin-data">
           <p className="coin-price">${price}</p>
-          <p className="coin-volum">${volume.toLocaleString()}</p>
+          <p className="coin-volume">${volume.toLocaleString()}</p>
+
+          {priceChange < 0 ? (
+            <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+          )}
+          <p className="coin-marketcap">
+            Mket Cap: ${marketcap.toLocaleString()}
+          </p>
         </div>
-        {priceChange < 0 ? (
-          <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
-        ) : (
-          <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
-        )}
-        <p className="coin-marketcap">
-          Mket Cap: ${marketcap.toLocaleString()}
-        </p>
       </div>
     </div>
   );
