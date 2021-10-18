c31c13e35790eaa4444bde5cae13260d52c1e5e5
Jason <kangja1996@gmail.com>
2021-10-18 01:05:20 -0400

    add a character

--- src/Coin.js
+++ src/Coin.js
@@ -20,7 +20,7 @@ const Coin = ({
         </div>
         <div className="coin-data">
           <p className="coin-price">${price}</p>
-          <p className="coin-volume">${volume.toLocaleString()}</p>
+          <p className="coin-volu me">${volume.toLocaleString()}</p>
 
           {priceChange < 0 ? (
             <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
@@ -37,3 +37,5 @@ const Coin = ({
 };
 
 export default Coin;
+
+s;
